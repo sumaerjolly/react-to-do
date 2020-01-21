b7b37db6e31405322125867e57fa7c2abf9a021f
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-21 18:57:32 +0530

    added few todos and option to delete

---
+++ src/Todos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Todos = ({ todos, deleteToDo }) => {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span
+            onClick={() => {
+              deleteToDo(todo.id);
+            }}
+          >
+            {' '}
+            {todo.content}{' '}
+          </span>
+        </div>
+      );
+    })
+  ) : (
+    <p className="center">You have no todo's left, yay!</p>
+  );
+  return <div className="todos collection">{todoList}</div>;
+};
+
+export default Todos;
