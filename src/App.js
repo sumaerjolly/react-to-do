1cb3015dfe603cd8f688fbaf63375c0a2861fc76
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-21 19:26:28 +0530

    added form for todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddForm from './AddForm';
 
 class App extends Component {
   state = {
@@ -17,11 +18,20 @@ class App extends Component {
       todos
     });
   };
+
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  };
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos deleteToDo={this.deleteTodo} todos={this.state.todos} />
+        <AddForm addTodo={this.addTodo} />
       </div>
     );
   }
